3abd18e569d26df04a0ef5aca5c5f1776855ff1f
Narongrit Chuangchu <90169790+narongritgithub@users.noreply.github.com>
2022-10-25 10:54:42 +0700

    SU Calculator Revise

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+
+echo "[user]" >> .git/config
+echo "    email = 15830559+puttsk@users.noreply.github.com" >> .git/config
+
+
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/puttsk/tara-su-calculator.git master:gh-pages
+
+cd -
